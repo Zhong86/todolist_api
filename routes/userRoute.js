f499f34ecb67486a6a7f34437b41c55a54bfe73d
Billy Zhong <billy3rlvin@gmail.com>
2025-12-30 09:45:55 +0700

    Finish Login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,22 @@
+require('dotenv').config(); 
 const express = require('express');
 const router = express.Router(); 
 const mongoose = require('mongoose');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-//GET ALL
-router.get('/users', async (req, res) => {
+//GET ONE
+router.get('/users', authenticateToken, async (req, res) => {
     try {
      const users = await User.find(); 
-     res.json(users);    
+     res.json(users.filter(user => user.email === req.user.email));    
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
-//CREATE
+//REGISTER
 router.post('/register', async (req, res) => {
     const hashedPass = await bcrypt.hash(req.body.password, 10); 
     const user = new User({
@@ -25,9 +27,52 @@ router.post('/register', async (req, res) => {
 
     try {
         const newUser = await user.save();
-        const newToken =  
-        res.status(201).json(newUser);
+
+        //return token from JWT
+        const userData = { name: newUser.name, email: newUser.email };
+        const newToken = jwt.sign(userData, process.env.TOKEN_SECRET);
+
+        res.status(201).json({ token: newToken });
     } catch (error) {
-        
+        res.status(400).json({message: error.message});
     }
 });
+
+//LOGIN
+router.post('/login/:id', async (req, res) => {
+    try {
+        const user = await User.find({ email: req.body.email });
+        if(user == null ) return res.status(404).json({ message: 'User not found' });
+
+        const pass = req.body.password; 
+        const correct = await bcrypt.compare(pass, user.password); 
+
+        if(correct) {
+            //generate token
+            const user = { name: user.name, email: user.email };
+            const token = jwt.sign(user, process.env.TOKEN_SECRET);
+            
+            res.status(200).json({ token: token });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; 
+    if (token == null) {
+        res.status(401).json({ message: 'No token provided' });
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user; 
+        next(); 
+    });  
+}
+
+module.exports = router; 
